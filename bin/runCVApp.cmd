2659d8047b3df0c199ff2cd72e58648a0f3b785a
jhu <jjhu19@gmaill.com>
2018-02-10 18:18:46 -0500

    Added bin for user to run application

---
+++ bin/runCVApp.cmd
@@ -0,0 +1,7 @@
+rem  Need java 1.7 and up. and java is in your path
+rem set JAVA_HOME=.\jre
+
+set CLASSPATH=.
+set CLASSPATH=%CLASSPATH%;ComputerVisionApp.jar
+
+java -classpath %CLASSPATH% com.jhu.app.ComputerVisionApp
---
+++ bin/runCVApp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#java 1.7 and up required. Make sure java is in your path
+# JAVA_HOME=.\jre
+
+MYCLASSPATH='ComputerVisionApp.jar'
+
+java -classpath $MYCLASSPATH com.jhu.app.ComputerVisionApp
+
+
